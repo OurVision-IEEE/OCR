8ccd90f02bd855cbcdf3e99bf375fc68ffc3e5da
Tejas M R <matblockgreninja@gmail.com>
2022-01-01 06:26:20 +0000

    Combined opencv with DocOCR

--- DocOCR.py
+++ DocOCR.py
@@ -1,12 +1,19 @@
+import cv2
 import pytesseract
 from PIL import Image
 
 class DocOCR:
-    def __init__(self, imagePath: str):
-        self.imagePath: str = imagePath
+    def __init__(self, imagePath:str=None, image=None):
+        if imagePath:
+            self.imagePath:str=imagePath
+            self.image=cv2.imread(imagePath)
+        if image:
+            self.image=image
+        assert self.image is not None
+        self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
 
     def ocr(self) -> str:
-        return pytesseract.image_to_string(Image.open(self.imagePath))
+        return pytesseract.image_to_string(self.image)
     
 if __name__ == "__main__":
     print(DocOCR('original-image.png').ocr())
